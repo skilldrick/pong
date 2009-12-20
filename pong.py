a5a6b44769d3277e6e9e7939c995e9603d3c49e3
Skilldrick <skilldrick@gmail.com>
2009-12-20 19:11:50 +0000

    Got my first Tkinter window!

--- pong.py
+++ pong.py
@@ -1,2 +1,18 @@
-#This is my pong.py file
-#This really is in the dev branch
+import Tkinter
+
+class MainWindow:
+    def __init__(self):
+        self.root = Tkinter.Tk()
+
+        w = Tkinter.Label(self.root, text='hello!')
+        w.pack()
+
+        self.root.mainloop()
+
+
+def main():
+    x = MainWindow()
+
+        
+if __name__ == '__main__':
+    main()
