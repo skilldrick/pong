1b4968adbfafac8078fadd7ff6f6dbe55d671c63
Skilldrick <skilldrick@gmail.com>
2010-01-09 00:23:35 +0000

    Added keybindings for up and down keys

--- pong.py
+++ pong.py
@@ -1,30 +1,37 @@
-import unittest
-import Tkinter
-
-class MainWindow:
-    def makeWindow(self):
-        self.root = Tkinter.Tk()
-
-        w = Tkinter.Label(self.root, text='hello!')
-        w.pack()
-
-        self.root.mainloop()
-
-        return True
-    
-    
+from tkinter import *
+from tkinter import ttk
 
 
 
-class MainWindowTests(unittest.TestCase):
-    def testMakeWindow(self):
-        mw = MainWindow()
-        self.failUnless(mw.makeWindow())
-    
+
+def registerEvents(root):
+    root.bind('<Key>', move)
+
+def move(event):
+    if event.keysym == 'Up':
+        print('Up')
+    elif event.keysym == 'Down':
+        print('Down')
 
 def main():
-    unittest.main()
+    root = Tk()
 
+    scoreFrame = ttk.Frame(root,
+                           borderwidth=3,
+                           relief='sunken',
+                           width=100, height=50)
+    scoreFrame.grid()
+
+    gameFrame = ttk.Frame(root,
+                          borderwidth=3,
+                          relief='sunken',
+                          width=100, height=100)
+    gameFrame.grid()
+
+    registerEvents(root)
+    
+    root.mainloop()
+    
         
 if __name__ == '__main__':
     main()
