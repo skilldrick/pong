85a9a6eb23bdac032832f4b509cf42f0051029b5
Skilldrick <skilldrick@gmail.com>
2010-01-09 22:46:25 +0000

    Before major refactor

--- pong.py
+++ pong.py
@@ -1,17 +1,35 @@
 from tkinter import *
 from tkinter import ttk
 
+BOARD_WIDTH = 100
+BOARD_HEIGHT = 100
+
+class Board:
+    def __init__(self, parent):
+        self.canvas = Canvas(parent,
+                             width=BOARD_WIDTH,
+                             height=BOARD_HEIGHT)
+        self.canvas.grid()
+        self.ball = Ball(self)
 
 
 
-def registerEvents(root):
-    root.bind('<Key>', move)
+class Paddle:
+    def __init__(self):
+        pass
 
-def move(event):
-    if event.keysym == 'Up':
-        print('Up')
-    elif event.keysym == 'Down':
-        print('Down')
+
+class Ball:
+    def __init__(self, board):
+        self.board = board
+        self.ball = self.board.canvas.create_rectangle((10, 10, 20, 20),
+                                                       fill='black')
+
+    def moveUp(self):
+        self.board.canvas.coords(self.ball, (10, 11, 12, 13))
+        
+
+
 
 def main():
     root = Tk()
@@ -19,18 +37,31 @@ def main():
     scoreFrame = ttk.Frame(root,
                            borderwidth=3,
                            relief='sunken',
-                           width=100, height=50)
+                           width=BOARD_WIDTH, height=50)
     scoreFrame.grid()
 
     gameFrame = ttk.Frame(root,
                           borderwidth=3,
-                          relief='sunken',
-                          width=100, height=100)
+                          relief='sunken')
     gameFrame.grid()
 
+
+
+    board = Board(gameFrame)
+
+    def registerEvents(root):
+        root.bind('<Key>', move)
+
+    def move(event):
+        if event.keysym == 'Up':
+            board.ball.moveUp()
+        elif event.keysym == 'Down':
+            print('Down')
+        
     registerEvents(root)
-    
     root.mainloop()
+
+    
     
         
 if __name__ == '__main__':
