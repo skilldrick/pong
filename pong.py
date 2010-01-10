4013977d86f605f02ee08282b4f5edc3209afe7b
Skilldrick <skilldrick@gmail.com>
2010-01-10 23:09:06 +0000

    Now the ball moves (but doesn't bounce)

--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 import time
+import math
 
 BOARD_WIDTH = 100
 BOARD_HEIGHT = 100
@@ -12,26 +13,21 @@ class Game:
     
     def __init__(self):
         self.root = Tk()
-        
         self.gameFrame = ttk.Frame(self.root,
                               borderwidth=3,
                               relief='sunken')
         self.gameFrame.grid()
-
         self.scoreFrame = ttk.Frame(self.gameFrame,
                                     borderwidth=3,
                                     relief='sunken',
                                     width=BOARD_WIDTH,
                                     height=50)
         self.scoreFrame.grid()
-
         self.buildBoard()
         self.items.update(ball=Ball(self.board))
         self.items.update(paddleL = PaddleL(self.board))
         self.items.update(paddleR = PaddleR(self.board))
-
         self.registerEvents()
-
         #self.root.update()
         self.gameLoop()
 
@@ -52,22 +48,21 @@ class Game:
             self.items['paddleR'].startUp()
         elif event.keysym == 'Down':
             self.items['paddleR'].startDown()
+        elif event.keysym == 'Right':
+            self.items['ball'].rotate()
 
     def keyUpEvent(self, event):
-        self.items['paddleR'].stop()
         if event.keysym == 'Up':
-            pass
-            #self.items['paddleR'].stop()
+            self.items['paddleR'].stop()
         elif event.keysym == 'Down':
-            pass
-            #self.items['paddleR'].stop()
-            
+            self.items['paddleR'].stop()
 
     def gameLoop(self):
         try:
             exit = False
             while not exit:
                 start_time = time.clock()
+                self.items['ball'].calcVelocity()
                 for item in self.items.values():
                     item.update()
                 self.root.update_idletasks()
@@ -85,28 +80,30 @@ class MovingObject:
     yvel = 0
 
     def startUp(self):
-        self.yvel = -1
+        self.yvel = -self.velocity
 
     def startDown(self):
-        self.yvel = 1
+        self.yvel = self.velocity
 
     def stop(self):
         self.xvel = 0
         self.yvel = 0
     
     def update(self):
-        self.move(self.xvel, self.yvel)
+        self.move()
     
-    def move(self, x, y):
+    def move(self):
         x1, y1, x2, y2 = self.board.coords(self.id)
-        x1 += x
-        x2 += x
-        y1 += y
-        y2 += y
-        self.board.coords(self.id, (x1, y1, x2, y2))
+        newcoords = (x1 + self.xvel,
+                     y1 + self.yvel,
+                     x2 + self.xvel,
+                     y2 + self.yvel)
+        self.board.coords(self.id, newcoords)
 
         
 class Paddle(MovingObject):
+    velocity = 2
+    
     def __init__(self, board, originX):
         self.board = board
         width = 5
@@ -116,12 +113,6 @@ class Paddle(MovingObject):
             (originX, originY,
              originX + width, originY + height),
             fill='black')
-
-    def moveUp(self):
-        self.move(0, -1)
-
-    def moveDown(self):
-        self.move(0, 1)
     
 
 class PaddleL(Paddle):
@@ -133,6 +124,9 @@ class PaddleR(Paddle):
         Paddle.__init__(self, board, 90)
 
 class Ball(MovingObject):
+    velocity = 0.5
+    angle = math.pi / 4
+    
     def __init__(self, board):
         self.board = board
         width = 5
@@ -143,6 +137,17 @@ class Ball(MovingObject):
             (originX, originY,
              originX + width, originY + height),
             fill='black')
+
+    def calcVelocity(self):
+        adj = math.cos(self.angle) * self.velocity #cos(theta) = adj/hyp
+        opp = math.sin(self.angle) * self.velocity #sin(theta) = opp/hyp
+        self.xvel = adj
+        self.yvel = opp
+
+    def rotate(self):
+        self.angle += math.pi / 4
+        
+    
 
         
 
